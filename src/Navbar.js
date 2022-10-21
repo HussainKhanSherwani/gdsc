1715014652361b346684b5964af975059e34b667
HussainKhanSherwani <ghulamhussainsherwani@gmail.com>
2022-10-21 22:30:32 +0500

    created navbar

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+function Navbar() {
+    return(
+    <nav>
+        <div>Navbar</div>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#help">Help</a></li>
+            <li><a href="#settings">Settings</a></li>
+
+        </ul>
+    </nav>);
+}
+export default Navbar;
